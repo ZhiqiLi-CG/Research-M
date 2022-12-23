16f4970e497c78bb86d633f4d114ebb7d0e26904
ZhiqiLi-CG <zhiqilicg@gmail.com>
2022-12-24 02:10:15 +0800

    add utils

---
+++ zqBasicUtils/zqBasic.cmake
@@ -0,0 +1,7 @@
+set(zqBasicUtils_DIR ${CMAKE_CURRENT_LIST_DIR})
+
+get_filename_component(zqBasicUtils_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/../ ABSOLUTE)
+set(zqBasicUtils_INCLUDE_DIRS
+	${zqBasicUtils_INCLUDE_DIR}
+	${PROJECT_BINARY_DIR})
+	
